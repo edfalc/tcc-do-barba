8979c7559cedcc816dd25a4e8bca878e0d96104a
Eduardo Falcão <eduardo.vieirafalcao@mail.utoronto.ca>
2020-09-02 15:18:34 -0300

    First Commit

---
+++ engine.py
@@ -0,0 +1,7 @@
+class Engine:
+    def __init_(self, lengthConnRod, crankRadius, pistonDiameter, clearanceVol):
+        self.lengthConnRod = lengthConnRod
+        self.crankRadius = crankRadius
+        self.pistonDiameter = pistonDiameter
+        self.clearanceVol = clearanceVol
+    pass
---
+++ main.py
@@ -0,0 +1,5 @@
+import math
+import engine as eng
+
+# Create engine object
+eng.Engine(lengthConnRod=183.2e-3, crankRadius=43.15e-3, pistonDiameter=70e-3, clearanceVol=32.63663e-6)
