5df706166e05bbe03a4132b0a0ceb17b3c476204
Eduardo Falcão <eduardo.vieirafalcao@mail.utoronto.ca>
2020-09-03 14:24:43 -0300

    Deleted engine.py

--- engine.py
+++
@@ -1,7 +0,0 @@
-class Engine:
-    def __init__(self, lengthConnRod, crankRadius, pistonDiameter, clearanceVol):
-        self.lengthConnRod = lengthConnRod
-        self.crankRadius = crankRadius
-        self.pistonDiameter = pistonDiameter
-        self.clearanceVol = clearanceVol
-        pass
